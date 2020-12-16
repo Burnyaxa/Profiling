be24a95c5699abe58dc10b8a4aa04b6b38de8e7a
Artem Supryhan <suprigan.artem1@gmail.com>
2020-12-16 22:45:54 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+
+namespace ProfilingLab
+{
+    class Program
+    {
+		static int func1(int a, int b)
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				int res = func2(a, b);
+				if (res > 0)
+					return res;
+			}
+
+			return 0;
+		}
+
+		static int func2(int a, int b)
+		{
+			int res = 0;
+		
+			for (int i = 0; i < 10; i++)
+			{
+				Thread.Sleep(1);
+				if (i > 8)
+					res = resultOfSum(a, b);
+				if (res > 0)
+					return res;
+			}
+			return res;
+		}
+		static int resultOfSum(int a, int b)
+		{
+			return a + b;
+		}
+
+		static void Main(string[] args)
+        {
+			func1(5, 10);
+
+        }
+    }
+}
